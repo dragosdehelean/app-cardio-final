4b68061dfbcc05d5b4edbbd6af2085957467b5ee
Dragos Dehelean <34175492+dragosdehelean@users.noreply.github.com>
2018-12-08 08:19:24 +0200

    Updates

---
+++ precache-manifest.74570ba6d94c84ffa1a3cbfa0480f436.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "0d580cd9df349c0db3de",
+    "url": "/app-cardio-final/static/js/runtime~main.0d580cd9.js"
+  },
+  {
+    "revision": "2f1cf6a7fa7b54f90718",
+    "url": "/app-cardio-final/static/js/main.2f1cf6a7.chunk.js"
+  },
+  {
+    "revision": "fe65e5159218a8a08e5e",
+    "url": "/app-cardio-final/static/js/1.fe65e515.chunk.js"
+  },
+  {
+    "revision": "d56375a5103bb7282723f2af905ba797",
+    "url": "/app-cardio-final/index.html"
+  }
+];
